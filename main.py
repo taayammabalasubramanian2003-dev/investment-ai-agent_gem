157caf667a7cc4236b66846bf9871a1a6485935c
taayammabalasubramanian2003-dev <taayammabalasubramanian2003@gmail.com>
2026-01-29 21:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import google.generativeai as genai
 import os
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-pro")
+#model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 
 # =========================
